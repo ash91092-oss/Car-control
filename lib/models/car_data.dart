b47ecfb28fd9a7989c18aaf4320882fcbceffad6
ash91092-oss <ash91092@gmail.com>
2026-07-25 09:41:53 +0330

    lib/models/car_data.dart

---
+++ lib/models/car_data.dart
@@ -0,0 +1,10 @@
+class CarData {
+  int rpm = 0;
+  int temp = 0;
+  int speed = 0;
+  double voltage = 0.0;
+  bool lightsOn = false;
+  String currentMode = 'NORMAL';
+  bool underlightOn = true;
+  bool backfireActive = false;
+}
